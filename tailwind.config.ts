d11fecd7f951c8a87ee20a644361d19434510750
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2023-12-11 19:28:07 +0100

    style: updated breakpoints in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,14 @@ module.exports = {
     darkMode: ['class'],
     content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
     theme: {
+        screens: {
+            xs: '520px',
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            '2xl': '1536px',
+        },
         container: {
             center: true,
             padding: '2rem',
