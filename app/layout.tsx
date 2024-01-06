046bdc6d5663a25fc699c6cedbd52dcbacfe580f
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2024-01-06 23:28:38 +0100

    style: updated import path for global styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
-import '../styles/globals.css';
+import '@/styles/globals.css';
 import AuthProvider from '@/components/AuthProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: 'Spotify Wrapped',
-    description:
-        'A web application that presents your Spotify music listening statistics in a visually appealing format.',
+    description: 'A web application that presents your Spotify music listening statistics in a visually appealing format.',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
