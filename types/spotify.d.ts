ec6871f17356df9275a8b15abc8170c9cdffa526
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2024-02-26 21:53:36 +0100

    feat: update ArtistItem and ArtistData interfaces

--- types/spotify.d.ts
+++ types/spotify.d.ts
@@ -1,2 +1,26 @@
 type Type = 'artists' | 'tracks';
 type Term = 'short_term' | 'medium_term' | 'long_term';
+
+interface Artist {
+    external_urls: {
+        spotify: string;
+    };
+    followers: {
+        href: string | null;
+        total: number;
+    };
+    genres: string[];
+    href: string;
+    id: string;
+    images: ImageObject[];
+    name: string;
+    popularity: number;
+    type: 'artist';
+    uri: string;
+}
+
+interface ImageObject {
+    height?: number | null;
+    url: string;
+    width?: number | null;
+}
