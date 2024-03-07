f676872d530443f67f95a8a7ac4d5dc81268cf10
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2024-03-07 21:06:13 +0100

    refactor: move AuthProvider to providers directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import '@/styles/globals.css';
-import AuthProvider from '@/components/AuthProvider';
+import AuthProvider from '@/providers/AuthProvider';
 import Nav from '@/components/Nav';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -14,9 +14,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <AuthProvider>
             <html lang="en">
                 <body className={inter.className}>
+                <AuthProvider>
                     <div className="max-h-screen h-screen xs:p-2 md:p-3 overflow-hidden">
                         <div className="w-full h-1/5 bg-cover bg-no-repeat bg-[url('/hero-mobile.jpg')] sm:bg-[url('/hero-desktop.jpg')] xs:max-w-7xl xs:mx-auto xs:rounded-[var(--radius)]">
                             <Nav />
@@ -24,8 +24,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
                         <main className="w-full h-4/5 flex justify-center mt-4">{children}</main>
                     </div>
+                </AuthProvider>
                 </body>
             </html>
-        </AuthProvider>
     );
 }
