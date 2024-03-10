84722e65a117b90329ff47eff17deaab6f87b507
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2024-03-10 16:21:55 +0100

    style: add drop shadow to HeroTitle component

--- components/HeroTitle.tsx
+++ components/HeroTitle.tsx
@@ -6,7 +6,7 @@ const HeroTitle = () => {
     const pathname = usePathname();
 
     return (
-        <h1 className="pb-3 text-white text-4xl font-semibold sm:pb-5 md:text-5xl lg:text-6xl">
+        <h1 className="pb-3 text-white text-4xl font-semibold sm:pb-5 md:text-5xl lg:text-6xl drop-shadow-xl">
             {pathname === '/' && 'Home'}
             {pathname === '/music' && 'Most Listened Music'}
             {pathname === '/artists' && 'Most Listened Artists'}
