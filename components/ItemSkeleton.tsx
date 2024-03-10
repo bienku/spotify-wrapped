005c5f8053f5d7519bc711843b25116f591fe166
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2024-03-10 18:02:49 +0100

    fix: implement dynamic widths for ItemSkeleton component

--- components/ItemSkeleton.tsx
+++ components/ItemSkeleton.tsx
@@ -1,12 +1,16 @@
+import { useState, useEffect } from 'react';
+
 import { Skeleton } from '@/components/ui/skeleton';
 
 const ItemSkeleton = () => {
-    const widths = [14, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60];
+    const widths = [3.5, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    const [randomWidth1, setRandomWidth1] = useState<number>(0);
+    const [randomWidth2, setRandomWidth2] = useState<number>(0);
 
-    const randomWidth = () => {
-        const width = widths[Math.floor(Math.random() * widths.length)];
-        return `w-${width}`;
-    };
+    useEffect(() => {
+        setRandomWidth1(widths[Math.floor(Math.random() * widths.length)]);
+        setRandomWidth2(widths[Math.floor(Math.random() * widths.length)]);
+    }, []);
 
     return (
         <div className="flex items-center text-sm space-x-4 py-2">
@@ -14,9 +18,8 @@ const ItemSkeleton = () => {
             <Skeleton className="w-12 h-12 rounded-[5px] bg-neutral-200" />
 
             <div className="flex flex-col">
-                {/* <Skeleton className="w-24 h-4 mb-2 bg-neutral-200 w-" /> */}
-                <Skeleton className={`h-4 mb-2 bg-neutral-200 ${randomWidth()}`} />
-                <Skeleton className={`h-4 bg-neutral-200 ${randomWidth()} `} />
+                <Skeleton style={{ width: `${randomWidth1}rem` }} className={`h-4 mb-2 bg-neutral-200`} />
+                <Skeleton style={{ width: `${randomWidth2}rem` }} className={`h-4 bg-neutral-200`} />
             </div>
         </div>
     );
