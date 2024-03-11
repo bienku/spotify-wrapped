c3024eded5da2447f41425643a7e1ffe7adecc28
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2024-03-11 17:48:15 +0100

    feat: create home page layout and add navigation buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
-interface Tab {
-    term: Term;
-    label: string;
-}
+import Link from 'next/link';
 
-const TABS: Tab[] = [
-    { term: 'short_term', label: 'last month' },
-    { term: 'medium_term', label: 'last 6 months' },
-    { term: 'long_term', label: 'all time' },
-];
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
-    return <h2>Welcome Page!</h2>;
+    return (
+        <div className="w-5/6 sm:w-2/3 h-full max-w-[853px] space-y-3 pt-[20vh]">
+            <h2 className="text-2xl font-semibold text-center xs:text-4xl sm:text-5xl lg:text-6xl">Unwrap Your Beats!</h2>
+            <p className="text-sm text-center md:text-base">
+                Navigate Through Your Most Played Tracks and Favorite Artists, and Let Your Tunes Tell Your Tale
+            </p>
+            <div className="flex justify-center space-x-4 pt-3">
+                <Button asChild>
+                    <Link href="/music">Music</Link>
+                </Button>
+                <Button asChild>
+                    <Link href="/artists">Artists</Link>
+                </Button>
+            </div>
+        </div>
+    );
 }
