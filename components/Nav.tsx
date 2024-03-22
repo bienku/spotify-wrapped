412b999034d252b5802a6df0f24294e5d40b3eb1
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2024-03-22 15:28:45 +0100

    style: update Nav component

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -23,15 +23,15 @@ const Nav = () => {
     const handleNav = () => setIsDrawerOpen((prevState) => !prevState);
 
     return (
-        <nav className="flex justify-end items-center h-12 max-w-[1240px] mx-auto text-black md:justify-center">
+        <nav className="flex justify-end items-center h-12 max-w-[1240px] mx-auto md:justify-center">
             {/* Desktop Navigation */}
             <ul
                 className={cn(
-                    'hidden relative h-12 rounded-b-[var(--radius)] bg-white px-2',
+                    'hidden relative h-12 rounded-b-[var(--radius)] bg-background px-2',
                     'xs:m-2',
                     'md:flex md:items-center md:m-3',
-                    'before:absolute before:-right-5 before:top-0 before:w-5 before:h-5 before:rounded-tl-[10px] before:shadow-[-5px_-5px_0_0_#fff]',
-                    'after:absolute after:-left-5 after:top-0 after:w-5 after:h-5 after:rounded-tr-[10px] after:shadow-[5px_-5px_0_0_#fff]',
+                    'before:absolute before:-right-5 before:top-0 before:w-5 before:h-5 before:rounded-tl-[10px] before:shadow-[-5px_-5px_0_0_#fff] dark:before:shadow-[-5px_-5px_0_0_#0c0a09]',
+                    'after:absolute after:-left-5 after:top-0 after:w-5 after:h-5 after:rounded-tr-[10px] after:shadow-[5px_-5px_0_0_#fff] dark:after:shadow-[5px_-5px_0_0_#0c0a09]',
                 )}
             >
                 {navItems.map((item) => (
@@ -57,8 +57,8 @@ const Nav = () => {
                 className={cn(
                     'relative text-black rounded-bl-[var(--radius)] h-12 w-12 flex items-center justify-center bg-background',
                     'md:hidden',
-                    'before:absolute before:right-12 before:top-0 before:w-5 before:h-5 before:rounded-tr-[10px] before:shadow-[5px_-5px_0_0_#fff]',
-                    'after:absolute after:right-0 after:top-12 after:w-5 after:h-5 after:rounded-tr-[10px] after:shadow-[5px_-5px_0_0_#fff]',
+                    'before:absolute before:right-12 before:top-0 before:w-5 before:h-5 before:rounded-tr-[10px] before:shadow-[5px_-5px_0_0_#fff] dark:before:shadow-[5px_-5px_0_0_#0c0a09]',
+                    'after:absolute after:right-0 after:top-12 after:w-5 after:h-5 after:rounded-tr-[10px] after:shadow-[5px_-5px_0_0_#fff] dark:after:shadow-[5px_-5px_0_0_#0c0a09]',
                 )}
             >
                 <BurgerButton isDrawerOpen={isDrawerOpen} handleNav={handleNav} />
@@ -67,7 +67,7 @@ const Nav = () => {
             {/* Mobile Navigation Drawer */}
             <ul
                 className={cn(
-                    'w-[60%] duration-500 fixed top-0 bottom-0 p-2 z-50',
+                    'w-[60%] duration-500 fixed top-0 bottom-0 p-2 space-y-3 z-50',
                     'md:hidden',
                     { 'left-0 h-full bg-[#000300] ease-in-out': isDrawerOpen },
                     { 'left-[-100%] ease-in-out': !isDrawerOpen },
