79bca14a631777dae854a05be6fc1cd3f3843985
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2024-03-28 13:39:14 +0100

    feat: add button to music page and update styles

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,18 +1,25 @@
 import { MusicProvider } from '@/hooks/useMusic';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import MusicList from '@/components/MusicList';
+import { Button } from '@/components/ui/button';
 import { TABS } from '@/constants';
 
 export default function Page() {
     return (
         <MusicProvider>
             <Tabs defaultValue="short_term" className="w-5/6 sm:w-2/3 h-full max-w-[853px]">
-                <TabsList className="grid w-full h-max grid-cols-3">
+                <TabsList className="grid w-full h-max grid-cols-[repeat(3,_minmax(0,_1fr)),_1.5rem] sm:grid-cols-[repeat(3,_minmax(0,_1fr)),_1.75rem] [&>*]:text-xs [&>*]:sm:text-sm">
                     {TABS.map(({ term, label }) => (
-                        <TabsTrigger key={term} value={term} className="text-xs sm:text-sm">
+                        <TabsTrigger key={term} value={term}>
                             {label}
                         </TabsTrigger>
                     ))}
+                    <Button
+                        size="icon"
+                        className="h-auto w-auto whitespace-nowrap rounded-md px-3 py-1 ring-offset-background transition-all hover:scale-125"
+                    >
+                        +
+                    </Button>
                 </TabsList>
 
                 {TABS.map(({ term }) => (
