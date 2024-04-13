7d7686522147876fb8aa431c944cb57de4e99683
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2024-04-13 19:45:20 +0200

    feat: add genres page component

---
+++ app/genres/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { ArtistsProvider } from '@/hooks/useArtists';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { TABS } from '@/constants';
+
+export default function Page() {
+    return (
+        <ArtistsProvider>
+            <Tabs defaultValue="short_term" className="w-5/6 sm:w-2/3 h-full max-w-[853px]">
+                <TabsList className="grid w-full h-max grid-cols-3">
+                    {TABS.map(({ term, label }) => (
+                        <TabsTrigger key={term} value={term} className="text-xs sm:text-sm">
+                            {label}
+                        </TabsTrigger>
+                    ))}
+                </TabsList>
+
+                {TABS.map(({ term }) => (
+                    <TabsContent key={term} value={term} className="h-[calc(100%-60px)] overflow-y-auto border rounded-md">
+                        {/* List */}
+                    </TabsContent>
+                ))}
+            </Tabs>
+        </ArtistsProvider>
+    );
+}
