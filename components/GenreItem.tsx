b14f27369d825070b2dbdcf9baae5c81f0a070fd
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2024-04-14 14:49:35 +0200

    feat: update GenreItem component to display count of artists with proper pluralization

--- components/GenreItem.tsx
+++ components/GenreItem.tsx
@@ -26,10 +26,12 @@ const GenreItem: React.FC<GenreItem> = ({ genre, count, totalGenresAmount, index
         >
             <p className="text-xs w-6 text-center">{index + 1}</p>
 
-            <div className="grid grid-cols-3 flex-1">
+            <div className="grid grid-cols-3 flex-1 pr-1">
                 <p>{genre}</p>
                 <p className="text-center">{percantage}%</p>
-                <p className="text-right">({count} artists)</p>
+                <p className="text-right">
+                    ({count} {count == 1 ? 'artist' : 'artists'})
+                </p>
             </div>
         </div>
     );
