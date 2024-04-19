eb5b9eb18a5c806b2ee3756b2571d4eb2cf9461d
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2024-04-19 21:13:02 +0200

    feat: update page.tsx to include genres in the navigation menu

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <div className="w-5/6 sm:w-2/3 h-full max-w-[853px] space-y-3 pt-[20vh]">
             <h2 className="text-2xl font-semibold text-center xs:text-4xl sm:text-5xl lg:text-6xl">Unwrap Your Beats!</h2>
             <p className="text-sm text-center md:text-base">
-                Navigate Through Your Most Played Tracks and Favorite Artists, and Let Your Tunes Tell Your Tale
+                Navigate Through Your Most Played Tracks, Favorite Artists, and Dominant Genres, and Let Your Tunes Tell Your Tale
             </p>
             <div className="flex justify-center space-x-4 pt-3">
                 <Button asChild>
@@ -15,6 +15,9 @@ export default function Home() {
                 </Button>
                 <Button asChild>
                     <Link href="/artists">Artists</Link>
+                </Button>
+                <Button asChild>
+                    <Link href="/genres">Genres</Link>
                 </Button>
             </div>
         </div>
